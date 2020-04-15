63d6ccbe4ab8c3cd9629d87a4998b6ec643616bb
Joel Parker Henderson <joel@joelparkerhenderson.com>
2020-04-14 17:29:37 -0700

    Rename file

--- hooks/pre-commit.d/file-name-must-not-end-with-dot-env.sh
+++ hooks/pre-commit.d/file-name-must-not-end-with-dot-env.sh
@@ -2,7 +2,7 @@
 set -euf
 if git diff --cached --name-only | grep '\.env$' > /dev/null; then
 	cat <<\EOF
-Error: Attempt to use a file name that ends with `.env`.
+Error: file name must not end with `.env`.
 These kinds of files may cause problems in git repositories because 
 they may contain environment variables such as passwords and keys.
 EOF
